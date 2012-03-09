284d3e58d90ca401ab52c300332a8de34d1d74f7
Antonio Garrote <antoniogarrote@gmail.com>
2012-03-09 11:11:54 +0000

    version bump in nodejs

--- configuration.rb
+++ configuration.rb
@@ -88,7 +88,7 @@ BUILD_CONFIGURATION = {
                 ],
     :package => {
       :name    => "rdfstore",
-      :version => "0.6.1",
+      :version => "0.6.2",
       :description => "RDF graph store supporting the SPARQL query language",
       :keywords => ["RDF", "SPARQL", "graph", "store"],
       :author  => {
